5ea050eca0f8709f6f3dbcf45e50cd1388e46251
doola-x <jackdulla47@gmail.com>
2024-07-30 03:07:56 +0000

    updating pathing for linux server

--- frontend/js/sharedlist.js
+++ frontend/js/sharedlist.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     function loadContent(page) {
-        fetch(`../components/${page}.html`)
+        fetch(`components/${page}.html`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
