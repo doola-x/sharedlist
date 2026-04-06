a39050932f7522aaab52189051f53a5a588f04af
doola-x <jackdulla47@gmail.com>
2026-04-06 04:25:33 +0000

    stack only for main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -26,11 +26,11 @@ int main(int argc, char **argv) {
 			return crow::response(400, res);
 		}
 
-		User *user = new User();
-		Util *util = new Util();
+		User user;
+		Util util;
 		
-		PassComponents pc = util->hashPassword(password);		
-		int result = user->signupUser(username, pc.hashword, pc.salt);
+		PassComponents pc = util.hashPassword(password);		
+		int result = user.signupUser(username, pc.hashword, pc.salt);
 		if (result == -1) {
 			res["status"] = "error";
 			res["msg"] = "the username or password was empty.";
@@ -53,11 +53,11 @@ int main(int argc, char **argv) {
 			res["msg"] = "the username or password was empty.";
 			return crow::response(400, res);
 		}
-		User *user = new User();
-		Util *util = new Util();
-		int result = user->loginUser(username, password);
+		User user;
+		Util util;
+		int result = user.loginUser(username, password);
 		if (result == 0) {
-			int session = util->createSession(username, req.get_header_value("X-Forwarded-For"));
+			int session = util.createSession(username, req.get_header_value("X-Forwarded-For"));
 			if (session) {
 				res["status"] = "failure";
 				return crow::response(400, res);
@@ -72,14 +72,14 @@ int main(int argc, char **argv) {
 
 	CROW_ROUTE(app, "/spotify_signin").methods("GET"_method)
 	([](const crow::request& req) {
-		Util *util = new Util();
-		User *user = new User();
+		Util util;
+		User user;
 		crow::json::wvalue res;
 		const string ip = req.get_header_value("X-Forwarded-For");
 		const string user_s = req.url_params.get("user") ? req.url_params.get("user") : "!error!";
-		vector<UserModel> users = util->getUser(user_s);
-		vector<SessionModel> sessions = util->getSessionFromUsername(user_s);
-		int session = util->hasValidSession(users[0].id, ip, sessions[0].session_file, user_s);
+		vector<UserModel> users = util.getUser(user_s);
+		vector<SessionModel> sessions = util.getSessionFromUsername(user_s);
+		int session = util.hasValidSession(users[0].id, ip, sessions[0].session_file, user_s);
 		if (session) {
 			res["status"] = "failure";
 			return crow::response(400, res);
@@ -90,8 +90,8 @@ int main(int argc, char **argv) {
 		const char* client_secret = getenv("SPOTIFY_CLIENT_SECRET");
 		if (client_id && client_secret) {
 			string url = "https://sharedlist.us/api/sso_callback";
-			string state = util->generateSalt(16);
-			int saved = user->recordState(user_s, state);
+			string state = util.generateSalt(16);
+			int saved = user.recordState(user_s, state);
 			string req_url = "https://accounts.spotify.com/authorize?";
 			string scope = "playlist-modify-private playlist-read-private user-read-currently-playing";
 			req_url += "response_type=code&client_id=" + string(client_id) + "&scope=" + scope + "&redirect_uri=" + url + "&state=" + state;
@@ -107,13 +107,13 @@ int main(int argc, char **argv) {
 
 	CROW_ROUTE(app, "/sso_callback").methods("GET"_method)
 	([](const crow::request& req) {
-	 	User *user = new User();
-		Util *util = new Util();
+	 	User user;
+		Util util;
 	 	crow::json::wvalue res;
 		string state = req.url_params.get("state") ? req.url_params.get("state") : "!error!";
         	string code = req.url_params.get("code") ? req.url_params.get("code") : "!error!";
 
-		SpotifyStateModel state_obj = user->fetchState(state);
+		SpotifyStateModel state_obj = user.fetchState(state);
 		if (state != state_obj.state) {
 			crow::json::wvalue res;
 			res["status"] = "failure";
@@ -123,10 +123,10 @@ int main(int argc, char **argv) {
 		const char* client_id = getenv("SPOTIFY_CLIENT_ID");
 		const char* client_secret = getenv("SPOTIFY_CLIENT_SECRET");
 		string post_data = "code=" + code + "&redirect_uri=" + url + "&grant_type=authorization_code";
-		string response = util->make_http_request("https://accounts.spotify.com/api/token", "POST", post_data, client_id, client_secret);
+		string response = util.make_http_request("https://accounts.spotify.com/api/token", "POST", post_data, client_id, client_secret);
 		crow::json::rvalue token = crow::json::load(response);
 
-		int updated = user->recordToken(state_obj.user_id, state_obj.state, token["access_token"].s());
+		int updated = user.recordToken(state_obj.user_id, state_obj.state, token["access_token"].s());
 		if (updated == -1) {
 			crow::json::wvalue res;
 			res["status"] = "failure";
@@ -142,12 +142,12 @@ int main(int argc, char **argv) {
 	([](const crow::request& req) {
 		auto body = crow::json::load(req.body);
 		string username = body["username"].s();
-		User *user = new User();
-		Util *util = new Util();
+		User user;
+		Util util;
 
-		vector<UserModel> users = util->getUser(username);
-		string access_token = user->fetchToken(users[0].id);
-		string response = util->make_http_request("https://api.spotify.com/v1/me/playlists?limit=12", "GET", "", "", "", access_token);
+		vector<UserModel> users = util.getUser(username);
+		string access_token = user.fetchToken(users[0].id);
+		string response = util.make_http_request("https://api.spotify.com/v1/me/playlists?limit=12", "GET", "", "", "", access_token);
 
 		crow::json::wvalue res;
 		bool done = false;
@@ -158,7 +158,7 @@ int main(int argc, char **argv) {
 		auto items_vec = items.lo();
 
 		while (!done) {
-			response = util->make_http_request(next.s(), "GET", "", "", "", access_token);
+			response = util.make_http_request(next.s(), "GET", "", "", "", access_token);
 			playlists = crow::json::load(response);
 			if (auto val = playlists["next"]; val.t() == crow::json::type::Null) {
 				done = true;
@@ -186,10 +186,10 @@ int main(int argc, char **argv) {
 			// missing params err
 		}
 
-		User *user = new User();
-		Util *util = new Util();
+		User user;
+		Util util;
 
-		vector<UserModel> users = util->getUser(username);
+		vector<UserModel> users = util.getUser(username);
 				
 		crow::json::wvalue res;
 		return crow::response(200, res);
@@ -202,12 +202,12 @@ int main(int argc, char **argv) {
 		string origin_type = body["origin_type"].s();
 		string origin_id = body["origin_id"].s();
 
-		User *user = new User();
-		Util *util = new Util();
-		Sharedlist *sharedlist = new Sharedlist();
+		User user;
+		Util util;
+		Sharedlist sharedlist;
 
-		vector<UserModel> users = util->getUser(username);
-		int result = sharedlist->createSharedlist(users[0].id, origin_type, origin_id);
+		vector<UserModel> users = util.getUser(username);
+		int result = sharedlist.createSharedlist(users[0].id, origin_type, origin_id);
 
 		crow::json::wvalue res;
 		if (result == -1) {
@@ -225,14 +225,13 @@ int main(int argc, char **argv) {
 		string username = body["username"].s();
 		string origin_id = body["origin_id"].s();
 
-		User *user = new User();
-		Util *util = new Util();
-		Sharedlist *sharedlist = new Sharedlist();
+		User user;
+		Util util;
+		Sharedlist sharedlist;
 
-		vector<UserModel> users = util->getUser(username);
-		string access_token = user->fetchToken(users[0].id);
-		cout << access_token << endl;
-		crow::json::rvalue res = sharedlist->syncSharedlist(access_token, origin_id);
+		vector<UserModel> users = util.getUser(username);
+		string access_token = user.fetchToken(users[0].id);
+		crow::json::rvalue res = sharedlist.syncSharedlist(access_token, origin_id);
 
 		crow::json::wvalue ret;
 		ret["res"] = res;
